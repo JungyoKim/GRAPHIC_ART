f4e479d3101249e0fb1ba31fe000f4241f9c3533
JungyoKim <58296133+JungyoKim@users.noreply.github.com>
2023-12-18 10:36:48 +0900

    Add files via upload

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,23 @@
+<script>
+    export let show = true;
+    export let title = '';
+    export let content = '';
+
+    function closeModal() {
+        show = false;
+    }
+</script>
+
+{#if show}
+<div class="fixed z-10 w-full inset-0 overflow-y-auto p-[50px]" role="dialog">
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="bg-white p-6 rounded shadow-lg text-center">
+            <h2 class="text-[80px] mb-4 font-bold">{title}</h2>
+            <p class="text-[40px]">{content}</p>
+            <div class="mt-4">
+                <button class="bg-gray-700 text-white px-4 py-2 rounded" on:click={closeModal}>닫기</button>
+            </div>
+        </div>
+    </div>
+</div>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,154 @@
+<script>
+    import Modal from '$lib/Modal.svelte';
+    let showModal = false;
+
+    function openModal() {
+        showModal = true;
+    }
+
+    openModal()
+</script>
+
+<Modal {showModal} title="4차원 공간 체험" content="ㅋㅋ" />
+
+
+<div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+   <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+        <h1></h1>
+    </div>
+    <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+        <h1></h1>
+    </div>
+    <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+        <h1></h1>
+    </div>
+    <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+        <h1></h1>
+    </div>
+    <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+        <h1></h1>
+    </div>
+    <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+        <h1></h1>
+    </div>
+</div>
+<div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+    <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+ </div>
+ <div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+    <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+ </div>
+ <div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+    <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+ </div>
+ <div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+    <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+ </div>
+ <div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+    <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+ </div>
+ <div class="animate-bounce animate-spin bg-gradient-to-r from-blue-500 to-red-500">
+    <div class="text-[100px] text-center bg-red-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-orange-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-yellow-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-green-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-blue-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+     <div class="text-[100px] text-center bg-purple-500 m-3 p-3 animate-spin text-white rounded-3xl font-bold">
+         <h1></h1>
+     </div>
+ </div>
